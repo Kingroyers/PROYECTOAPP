906e17bf0f5e93ce77da42b89a61e22a8bef8068
antonio <antonioroyero12@gmail.com>
2025-04-02 19:23:00 -0500

    actualizacion componentes

--- dashboard.php
+++ dashboard.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <link rel="stylesheet" href="src/css/bootstrap.min.css">
     <link rel="stylesheet" href="src/estilos.css">
-    <title>Dashboard</title>
+    <title>Dashboard-APP</title>
 </head>
 
 <body class="d-flex position-relative align-items-center bg-white flex-column">
-    <!---Barra principal-->
 
+    <!---Barra principal-->
     <?php include '../ProyectoAPP/View/componentes/barraPrincipal.html' ?>
 
     <div class=" d-flex justify-content-center align-items-center  w-100 ">
 
-        <!--------------------------------------------------------------------------------->
+        
         <div class="container-xxl position-relative d-flex flex-column">
 
 
@@ -27,175 +26,10 @@
                 <p style="margin: 30px 0 0 10px;">proximas clases</p>
                 <hr style="margin: 0;">
 
-                <div class="row d-flex justify-content-center align-content-center"
-                    style=" margin-top: 0; padding: 0 0 10px 0;">
+                <div class="row d-flex justify-content-center align-content-center" style=" margin-top: 0; padding: 0 0 10px 0;">
+                    
+                    <?php include '../ProyectoAPP/View/componentes/ProximasClases.html' ?>
 
-                    <!------------------------------------------------------------------------------->
-                    <div class="container-fluid m-3 w-100 w-sm-100">
-
-                        <div class="wrapper d-flex" style="max-height: 120px;  overflow-x: auto; height: 77px;">
-
-
-                            <a href="#" style="text-decoration: none; color: #000;">
-                                <div class="custom-proximas-clases item">
-
-                                    <div class="row" style=" width: 100%;">
-
-                                        <div class="col-4"
-                                            style="display: flex; padding: 0; justify-content: center; align-items: center;">
-                                            <img src="../src/img/running.png" alt="icons-running" width="40px"
-                                                height="40px">
-                                        </div>
-
-                                        <div class="col-8"
-                                            style="display: flex; justify-content: center; align-items: center;">
-                                            <h6>Cardio</h6>
-
-                                        </div>
-
-                                    </div>
-
-                                </div>
-                            </a>
-
-                            <a href="#" style="text-decoration: none; color: #000;">
-                                <div class="custom-proximas-clases item">
-
-                                    <div class="row" style=" width: 100%;">
-
-                                        <div class="col-4"
-                                            style="display: flex; padding: 0; justify-content: center; align-items: center;">
-                                            <img src="../src/img/inconos/iconos-spinning.png" alt="icons-running"
-                                                width="30px" height="30px">
-                                        </div>
-
-                                        <div class="col-8"
-                                            style="display: flex; justify-content: center; align-items: center;">
-                                            <h6>Spinning</h6>
-
-                                        </div>
-
-                                    </div>
-
-                                </div>
-                            </a>
-
-                            <a href="#" style="text-decoration: none; color: #000;">
-                                <div class="custom-proximas-clases item">
-
-                                    <div class="row" style=" width: 100%;">
-
-                                        <div class="col-4"
-                                            style="display: flex; padding: 0; justify-content: center; align-items: center;">
-                                            <img src="../src/img/inconos/icons-boxing.png" alt="icons-running"
-                                                width="30px" height="30px">
-                                        </div>
-
-                                        <div class="col-8"
-                                            style="display: flex; justify-content: center; align-items: center;">
-                                            <h6>Boxing</h6>
-
-                                        </div>
-
-                                    </div>
-
-                                </div>
-                            </a>
-
-                            <a href="#" style="text-decoration: none; color: #000;">
-                                <div class="custom-proximas-clases item">
-
-                                    <div class="row" style=" width: 100%;">
-
-                                        <div class="col-4"
-                                            style="display: flex; padding: 0; justify-content: center; align-items: center;">
-                                            <img src="../csrc/img/inconos/icons-zumba.png" alt="icons-running"
-                                                width="30px" height="30px">
-                                        </div>
-
-                                        <div class="col-8"
-                                            style="display: flex; justify-content: center; align-items: center;">
-                                            <h6>Zumba</h6>
-
-                                        </div>
-
-                                    </div>
-
-
-                                </div>
-                            </a>
-
-                            <a href="#" style="text-decoration: none; color: #000;">
-                                <div class="custom-proximas-clases item">
-
-                                    <div class="row" style=" width: 100%;">
-
-                                        <div class="col-4"
-                                            style="display: flex; padding: 0; justify-content: center; align-items: center;">
-                                            <img src="../src/img/Icons-Biceps.png" alt="icons-running" width="30px"
-                                                height="30px">
-                                        </div>
-
-                                        <div class="col-8"
-                                            style="display: flex; justify-content: center; align-items: center;">
-                                            <h6 style="font-size: 14px;">IronCWout</h6>
-
-                                        </div>
-
-                                    </div>
-
-                                </div>
-                            </a>
-
-                            <a href="#" style="text-decoration: none; color: #000;">
-                                <div class="custom-proximas-clases item">
-
-                                    <div class="row" style=" width: 100%;">
-
-                                        <div class="col-4"
-                                            style="display: flex; padding: 0; justify-content: center; align-items: center;">
-                                            <img src="../src/img/icons-peso-rusa.png" alt="icons-running" width="40px"
-                                                height="40px">
-                                        </div>
-
-                                        <div class="col-8"
-                                            style="display: flex; justify-content: center; align-items: center;">
-                                            <h6>CrossFit</h6>
-
-                                        </div>
-
-                                    </div>
-
-                                </div>
-                            </a>
-
-                            <a href="#" style="text-decoration: none; color: #000;">
-                                <div class="custom-proximas-clases item">
-
-                                    <div class="row" style=" width: 100%;">
-
-                                        <div class="col-4"
-                                            style="display: flex; padding: 0; justify-content: center; align-items: center;">
-                                            <img src="../src/img/Icons-Yoga.png" alt="icons-running" width="40px"
-                                                height="40px">
-                                        </div>
-
-                                        <div class="col-8"
-                                            style="display: flex; justify-content: center; align-items: center;">
-                                            <h6>Yoga</h6>
-
-                                        </div>
-
-                                    </div>
-
-                                </div>
-                            </a>
-                            <div class="custom-proximas-clases item">
-                                8</div>
-
-                        </div>
-                    </div>
-                    <!-------------------------------------------------------------------------------->
 
                     <p style="margin: 10px 0 10px 0;">Nuestra coleccion</p>
                     <hr>
@@ -206,8 +40,7 @@
                         <figure class="cartas-rutinas col-12 col-sm-4">
                             <a href="https://www.youtube.com/watch?v=oCtrksNCiKk"
                                 style="color: #101116; text-decoration: none;">
-                                <div class="fondo-rutina col10 col-md-4 d-flex flex-row justify-content-between"
-                                    style="border-radius: 10px; box-shadow: 5px 5px 35px #3f55762f; width: 100%;">
+                                <div class="fondo-rutina col10 col-md-4 d-flex flex-row justify-content-between" style="border-radius: 10px; box-shadow: 5px 5px 35px #3f55762f; width: 100%;">
 
                                     <div class="" style=" margin: 10px 10px ; z-index: 100;">
                                         <p style="margin-top: 70px; margin-left: 10px; color: #fff; font-size: 20px;">
@@ -216,7 +49,7 @@
                                     </div>
 
                                     <div class="position-relative"><img class="img-fluid
-                                     position-absolute bottom-0" src="../src/img/espalda.png" alt=""
+                                     position-absolute bottom-0" src="src/img/espalda.png" alt=""
                                             style="min-width: 70px; max-width: 140px; right: 0; "></div>
 
                                 </div>
@@ -224,10 +57,8 @@
                         </figure>
 
                         <figure class="cartas-rutinas col-12 col-sm-4">
-                            <a href="https://www.youtube.com/watch?v=VB09kLgJDo0"
-                                style="color: #101116; text-decoration: none;">
-                                <div class="fondo-rutina  col10 col-md-4 d-flex flex-row justify-content-between"
-                                    style=" max-height: 160px; border-radius: 10px; background-color: #101116; box-shadow: 5px 5px 35px #3f55762f; width: 100%;">
+                            <a href="https://www.youtube.com/watch?v=VB09kLgJDo0"  style="color: #101116; text-decoration: none;">
+                                <div class="fondo-rutina  col10 col-md-4 d-flex flex-row justify-content-between" style=" max-height: 160px; border-radius: 10px; background-color: #101116; box-shadow: 5px 5px 35px #3f55762f; width: 100%;">
 
                                     <div class=" d-flex flex-wrap" style=" margin: 10px 10px ; z-index: 100;">
                                         <p class="text-white"
@@ -237,7 +68,7 @@
                                     </div>
 
                                     <div class="position-relative"><img class="img-fluid
-                                        position-absolute bottom-0" src="../src/img/Abdomen-fotos.png" alt=""
+                                        position-absolute bottom-0" src="src/img/Abdomen-fotos.png" alt=""
                                             style="height: 100%; min-width: 70px; max-width: 200px; right: 0; mask-image: linear-gradient(black 89%, transparent);  filter: drop-shadow(0 0 10px rgb(0, 0, 0));">
                                     </div>
                                 </div>
@@ -246,10 +77,8 @@
                         </figure>
 
                         <figure class="cartas-rutinas col-12 col-sm-4">
-                            <a href="https://www.youtube.com/watch?v=_hkUNgX9e54"
-                                style="color: #101116; text-decoration: none;">
-                                <div class="fondo-rutina col10 col-md-4 d-flex flex-row justify-content-between"
-                                    style="border-radius: 10px; box-shadow: 5px 5px 35px #3f55762f; width: 100%;">
+                            <a href="https://www.youtube.com/watch?v=_hkUNgX9e54" style="color: #101116; text-decoration: none;">
+                                <div class="fondo-rutina col10 col-md-4 d-flex flex-row justify-content-between"  style="border-radius: 10px; box-shadow: 5px 5px 35px #3f55762f; width: 100%;">
 
                                     <div class="" style=" margin: 10px 10px ; z-index: 100;">
                                         <p style="margin-top: 70px; margin-left: 10px; color: #fff; font-size: 20px;">
@@ -258,7 +87,7 @@
                                     </div>
 
                                     <div class="position-relative"><img class="img-fluid
-                                     position-absolute bottom-0" src="../src/img/biceps-fotos.png" alt=""
+                                     position-absolute bottom-0" src="src/img/biceps-fotos.png" alt=""
                                             style=" min-width: 90px; max-width: 240px; right: 0; mask-image: linear-gradient(black 89%, transparent);  filter: drop-shadow(0 0 10px rgb(0, 0, 0));">
                                     </div>
 
@@ -279,7 +108,7 @@
                                     </div>
 
                                     <div class="position-relative"><img class="img-fluid
-                                     position-absolute bottom-0" src="../src/img/Abdomen.png" alt=""
+                                     position-absolute bottom-0" src="src/img/Abdomen.png" alt=""
                                             style=" min-width: 40px; max-width: 220px; right: 0; mask-image: linear-gradient(black 89%, transparent);  filter: drop-shadow(0 0 10px rgb(0, 0, 0));">
                                     </div>
 
@@ -287,17 +116,15 @@
                             </a>
                         </figure>
 
-
-
                     </div>
 
 
-                    <!-------------------------------------------------------------------------------->
+                    
                     <p style="margin: 30px 0 10px 0;">Nuestro Equipo</p>
                     <hr>
 
-                    <?php include '../ProyectoAPP/View/componentes/NuestroEquipo.html' ?>
-                    <!-------------------------------------------------------------------------------->
+                    <?php include '../ProyectoAPP/View/componentes/NuestroEquipo.php' ?>
+                </div>    
 
             </main>
 
@@ -305,15 +132,12 @@
 
 
 
-            <!-------------------------------------------- menu ----------------------------------->
+            <!-------------- menu ---------------->
             <?php include '../ProyectoAPP/View/componentes/menu.html' ?>
-            <!------------------------------------------------------------------------------->
+            
         </div>
 
-
         <script src="../ProyectoAPP/src/js/bootstrap.min.js"></script>
-
-
 </body>
 
 </html>
