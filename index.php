45a4440e42ca42cb7571c3c464f6c6ae78c4d6ea
YxcidDev <ederyarevalo@gmail.com>
2025-04-16 18:54:58 -0500

    Prueba

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,15 +9,17 @@
     <title>login</title>
 </head>
 </head>
+
 <body class="fondoinicio text-white">
 
     <section class="container d-flex align-items-center justify-content-center vh-100">
         <div class="row w-100 text-center text-lg-start">
-            
+
             <div class="col-lg-6 d-flex justify-content-center">
-                <img src="src/img/login2.png" alt="" class="imageninicio img-fluid mt-lg-4" style="width: 70vh; filter: drop-shadow(0px 0px 10px rgba(45, 49, 71, 0.144));">
+                <img src="src/img/login2.png" alt="" class="imageninicio img-fluid mt-lg-4"
+                    style="width: 70vh; filter: drop-shadow(0px 0px 10px rgba(45, 49, 71, 0.144));">
             </div>
-             
+
 
             <div class=" col-lg-6 d-flex flex-column align-items-center justify-content-center p- rounded">
 
@@ -24,17 +27,21 @@
 
                     <h1 class="fs-1 " style="font-family:  Valorant, sans-serif">FITNESS LIFE</h1>
                     <p class="d-none d-md-block z-lg-1 ">FITNESS LIFE, La mejor version de ti</p>
-                
-                  <div class="d-flex mt-4 justify-content-center">
-                    <a href="../ProyectoAPP/View/login.php"><button class="btn btn-outline-light px-4 py-2 rounded-" style="border-radius:  0 0 0 30px;">Iniciar</button></a>
-                    <a href="../ProyectoAPP/View/Register.php"><button class="btn btn-outline-light px-4 py-2 rounded-top-end" style="border-radius:  0 30px 0 0;">Resgistrar</button></a>
-                  </div>
+
+                    <div class="d-flex mt-4 justify-content-center">
+                        <a href="../ProyectoAPP/View/login.php"><button class="btn btn-outline-light px-4 py-2 rounded-"
+                                style="border-radius:  0 0 0 30px;">Iniciar</button></a>
+                        <a href="../ProyectoAPP/View/Register.php"><button
+                                class="btn btn-outline-light px-4 py-2 rounded-top-end"
+                                style="border-radius:  0 30px 0 0;">Resgistrar</button></a>
+                    </div>
                 </div>
-                
+
             </div>
-            <!----------------------------------------------------->
+            <!-------------------------Hola mundo---------------------------->
         </div>
     </section>
     <script src="/src/js/bootstrap.min.js"></script>
 </body>
+
 </html>
